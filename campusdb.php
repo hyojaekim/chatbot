4b9b699ac459d4eba83c497104e57bcef81fd4e5
kimhyojae <41109150+hyojaekim@users.noreply.github.com>
2018-10-09 12:26:32 +0900

    v2.1.1
    DB 간략화

--- campusdb.php
+++ campusdb.php
@@ -1,22 +1,5 @@
 <?php
-function user_DB($user) {
-    $db_host = "localhost"; 
-    $db_user = "DB_ID"; 
-    $db_passwd = "DB_PASSWORD";
-    $db_name = "DB_NAME";
-
-    // MySQL - DB 접속.
-    $conn = mysqli_connect($db_host,$db_user,$db_passwd,$db_name);
-
-    // 문자셋 설정, utf8.
-    mysqli_set_charset($conn,"utf8"); 
-
-    // 테이블 쿼리 후 내용 삽입(유저키)
-    $sql = "insert into campus (user) values ('$user')";
-    $result = mysqli_query($conn,$sql);
-}
-
-function numberUpdate_DB($user, $number) {
+function user_DB($user, $number) {
     $db_host = "localhost"; 
     $db_user = "rlagywo23"; 
     $db_passwd = "goddog123";
@@ -24,9 +7,13 @@ function numberUpdate_DB($user, $number) {
 
     // MySQL - DB 접속.
     $conn = mysqli_connect($db_host,$db_user,$db_passwd,$db_name);
-
+    
     // 문자셋 설정, utf8.
     mysqli_set_charset($conn,"utf8"); 
+
+    // 테이블 쿼리 후 내용 삽입(유저키)
+    $sql = "insert into campus (user) values ('$user')";
+    $result = mysqli_query($conn,$sql);
 
     // 테이블 쿼리 후 내용 수정(number), 해당 user의 number를 바꿔라
     $sql = "UPDATE campus SET number = '$number' WHERE user = '$user'";
