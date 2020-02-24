e5dc54f859e8dd07c288e3cf845e8842d75d955d
hyojaekim <hj00686@naver.com>
2020-02-25 03:12:33 +0900

    feat: IndexController 추가

--- src/test/kotlin/com/kdu/common/controller/IndexControllerTest.java
+++ src/test/kotlin/com/kdu/common/controller/IndexControllerTest.java
@@ -5,25 +5,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class HomeControllerTest {
+class IndexControllerTest {
 
     @Autowired
     private WebTestClient webTestClient;
 
     @Test
-    void 정상적으로_홈으로_이동한다() {
-        String result = webTestClient.get()
-                .uri("/home")
+    void index() {
+        webTestClient.get()
+                .uri("/")
                 .exchange()
-                .expectStatus()
-                .isOk()
-                .expectBody(String.class)
-                .returnResult()
-                .getResponseBody();
-
-        assertThat(result).isEqualTo("Hello World!");
+                .expectStatus().isOk();
     }
 }
