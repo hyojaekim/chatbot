ec65489cf469cba12b194144d08c2e32ef96d8be
hyojaekim <hj00686@naver.com>
2020-03-02 17:40:16 +0900

    feat: 외부 설정 파일 설정

--- src/main/kotlin/com/kdu/KduApplication.kt
+++ src/main/kotlin/com/kdu/KduApplication.kt
@@ -1,11 +1,21 @@
 package com.kdu
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
+@EnableJpaAuditing
 @SpringBootApplication
 class KduApplication
 
+private const val PROPERTIES = (
+        "spring.config.location="
+                + "classpath:application.yml,"
+                + "/app/config/springboot-webservice/real-application.yml"
+        )
+
 fun main(args: Array<String>) {
-	runApplication<KduApplication>(*args)
+    SpringApplicationBuilder(KduApplication::class.java)
+            .properties(PROPERTIES)
+            .run(*args)
 }
