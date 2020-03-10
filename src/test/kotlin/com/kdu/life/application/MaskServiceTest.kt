047f0a02b38082487cc43c3a6847473f6cf5596c
hyojaekim <hj00686@naver.com>
2020-03-10 18:26:52 +0900

    test: MaskService 실패하는 테스트 수정

--- src/test/kotlin/com/kdu/life/application/MaskServiceTest.kt
+++ src/test/kotlin/com/kdu/life/application/MaskServiceTest.kt
@@ -5,10 +5,11 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import java.time.Year
 
-internal class MaskServiceTest(val maskService: MaskService) {
+internal class MaskServiceTest {
 
     @Test
     internal fun `정상적으로 마스크 구매일을 알 수 있다`() {
+        val maskService = MaskService()
         val maskPurchaseInfoRequestDto = MaskPurchaseInfoRequestDto(Year.of(1991))
 
         assertThat(maskService.findDay(maskPurchaseInfoRequestDto)).isEqualTo("월")
