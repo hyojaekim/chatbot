ac27f0edabe6fab075195736da8b016117a5b480
hyojaekim <hj00686@naver.com>
2020-03-17 15:21:51 +0900

    feat: Cafeteria Enum 추가

---
+++ src/main/kotlin/com/kdu/food/domain/Cafeteria.kt
@@ -0,0 +1,28 @@
+package com.kdu.food.domain
+
+enum class Cafeteria(private val location: String, private val realName: String, private val code: String) {
+    YANGJU("양주", "캠퍼스 식당", "C04"),
+    YANGJU_HAPPY_SHARE("양주", "행복공공기숙사 식당", "C07"),
+    MUNMAK("문막", "캠퍼스 식당", "C05"),
+    MUNMAK_HAPPY_SHARE("문막", "행복공공기숙사 식당", "C06"),
+    GOSEONG("고성", " 캠퍼스 식당", "C01"),
+    GOSEONG_GIRL_DORMITORY("고성", " 숭례원 식당", "C02"),
+    GOSEONG_GIRL_STUDENT_DORMITORY("고성", " 숭례원 학생식당", "c08"),
+    GOSEONG_MAN_DORMITORY("고성", " 양현원 식당", "C03"),
+    EMPTY("", "", "");
+
+    override fun toString(): String {
+        return "$location$realName"
+    }
+
+    companion object {
+        fun findByCode(code: String): Cafeteria {
+            for (cafeteria in values()) {
+                if (cafeteria.code == code) {
+                    return cafeteria
+                }
+            }
+            return EMPTY
+        }
+    }
+}
---
+++ src/test/kotlin/com/kdu/food/domain/CafeteriaTest.kt
@@ -0,0 +1,25 @@
+package com.kdu.food.domain
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+internal class CafeteriaTest {
+
+    @Test
+    internal fun `캠퍼스 코드로 Cafeteria를 찾는다`() {
+        Assertions.assertThat(Cafeteria.findByCode("C01")).isEqualTo(Cafeteria.GOSEONG)
+        Assertions.assertThat(Cafeteria.findByCode("C02")).isEqualTo(Cafeteria.GOSEONG_GIRL_DORMITORY)
+        Assertions.assertThat(Cafeteria.findByCode("C03")).isEqualTo(Cafeteria.GOSEONG_MAN_DORMITORY)
+        Assertions.assertThat(Cafeteria.findByCode("C04")).isEqualTo(Cafeteria.YANGJU)
+        Assertions.assertThat(Cafeteria.findByCode("C05")).isEqualTo(Cafeteria.MUNMAK)
+        Assertions.assertThat(Cafeteria.findByCode("C06")).isEqualTo(Cafeteria.MUNMAK_HAPPY_SHARE)
+        Assertions.assertThat(Cafeteria.findByCode("C07")).isEqualTo(Cafeteria.YANGJU_HAPPY_SHARE)
+        Assertions.assertThat(Cafeteria.findByCode("c08")).isEqualTo(Cafeteria.GOSEONG_GIRL_STUDENT_DORMITORY)
+        Assertions.assertThat(Cafeteria.findByCode("~~")).isEqualTo(Cafeteria.EMPTY)
+    }
+
+    @Test
+    internal fun `정상적으로 이름을 가져온다`() {
+        Assertions.assertThat(Cafeteria.GOSEONG.toString()).isEqualTo("고성 캠퍼스 식당")
+    }
+}
