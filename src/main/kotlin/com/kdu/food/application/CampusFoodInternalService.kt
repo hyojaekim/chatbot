f107562f2c63b298f83d55ea3598fa393d64f71a
hyojaekim <hj00686@naver.com>
2020-03-20 22:43:00 +0900

    refactor: 학식 메뉴 저장 API 트랜잭션 적용

--- src/main/kotlin/com/kdu/food/application/CampusFoodInternalService.kt
+++ src/main/kotlin/com/kdu/food/application/CampusFoodInternalService.kt
@@ -4,6 +4,7 @@ import com.kdu.food.domain.Cafeteria
 import com.kdu.food.domain.CampusFoodRepository
 import com.kdu.food.domain.CampusFoods
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class CampusFoodInternalService(
@@ -11,6 +12,7 @@ class CampusFoodInternalService(
         private val campusFoodRepository: CampusFoodRepository
 ) {
 
+    @Transactional
     fun saveFoods(cafeteria: Cafeteria) {
         val crawlingCampusFoods = campusFoodCrawlingService.crawling(cafeteria)
         for (campusFoods in crawlingCampusFoods) {
