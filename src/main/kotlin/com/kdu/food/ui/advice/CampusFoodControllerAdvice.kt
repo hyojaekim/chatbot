77e2890dc3a1a106296dae196bf0face486dbac5
hyojaekim <hj00686@naver.com>
2020-03-23 15:41:22 +0900

    feat: 학식을 찾지 못하는 경우 메세지 전송

--- src/main/kotlin/com/kdu/food/ui/advice/CampusFoodControllerAdvice.kt
+++ src/main/kotlin/com/kdu/food/ui/advice/CampusFoodControllerAdvice.kt
@@ -1,8 +1,11 @@
 package com.kdu.food.ui.advice
 
+import com.kdu.common.message.ResponseMessage
 import com.kdu.food.exception.CampusFoodCrawlingFailException
 import com.kdu.food.exception.NotFoundCafeteriaException
+import com.kdu.food.exception.NotFoundCampusFoodException
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -13,5 +16,13 @@ class CampusFoodControllerAdvice {
     @ExceptionHandler(CampusFoodCrawlingFailException::class, NotFoundCafeteriaException::class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     fun handleCampusFoodCrawlingFailException() {
+    }
+
+    @ExceptionHandler(NotFoundCampusFoodException::class)
+    fun handleCampusFoodCrawlingFailException2(): ResponseEntity<String> {
+        return ResponseEntity.ok(ResponseMessage.Builder()
+                .simpleText("현재 메뉴가 정해지지 않았습니다.")
+                .build()
+                .toString())
     }
 }
