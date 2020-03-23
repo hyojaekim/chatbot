84a7b62ed6b42d53608d208f75ad2f530b163991
hyojaekim <hj00686@naver.com>
2020-03-23 15:36:54 +0900

    refactor: data-test.sql 변경

--- src/test/resources/data-test.sql
+++ src/test/resources/data-test.sql
@@ -1,1 +1,1 @@
-INSERT INTO campus_food(id, cafeteria, day_of_week, start_date, food_type, food_menu) VALUES (1, 0, '월', '2020-03-22', '한정식', '테스트 메뉴');
+INSERT INTO campus_food(id, cafeteria, day_of_week, start_date, food_type, food_menu) VALUES (1, 0, '월', NOW(), '한정식', '테스트 메뉴');
