0b8b12342930cfa0ba0ba1be0e3e607de94b2909
hyojaekim <hj00686@naver.com>
2020-03-26 18:11:49 +0900

    refactor: 캠퍼스 설정하지 않은 경우 메세지 전송

--- src/main/kotlin/com/kdu/user/presentation/advice/UserInfoApiControllerAdvice.kt
+++ src/main/kotlin/com/kdu/user/presentation/advice/UserInfoApiControllerAdvice.kt
@@ -1,7 +1,10 @@
 package com.kdu.user.presentation.advice
 
+import com.kdu.common.message.ResponseMessage
 import com.kdu.user.exception.NotFoundCampusException
+import com.kdu.user.exception.NotFoundUserException
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -12,5 +15,13 @@ class UserInfoApiControllerAdvice {
     @ExceptionHandler(NotFoundCampusException::class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     fun handleNotFoundCampusException() {
+    }
+
+    @ExceptionHandler(NotFoundUserException::class)
+    fun handleNotFoundUserException(): ResponseEntity<String> {
+        return ResponseEntity.ok(ResponseMessage.Builder()
+                .simpleText("캠퍼스를 설정하면 정상적으로 이용이 가능합니다.")
+                .build()
+                .toString())
     }
 }
