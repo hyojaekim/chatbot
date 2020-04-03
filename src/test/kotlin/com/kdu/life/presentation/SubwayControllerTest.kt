dbdea9b422dd782cec92bb78f9fb1d31c2613262
hyojaekim <hj00686@naver.com>
2020-04-03 21:38:01 +0900

    test: 지하철 도착정보 MockMvc 적용
    - 실패하는 테스트 추가

--- src/test/kotlin/com/kdu/life/presentation/SubwayControllerTest.kt
+++ src/test/kotlin/com/kdu/life/presentation/SubwayControllerTest.kt
@@ -1,24 +1,75 @@
 package com.kdu.life.presentation
 
+import com.kdu.common.message.BasicCard
+import com.kdu.common.resolver.SubwayInfoArgumentResolver
+import com.kdu.life.application.SubwayService
+import com.kdu.life.exception.NotFoundSubwayException
+import com.kdu.life.presentation.advice.SubwayControllerAdvice
 import com.kdu.util.JsonFactory
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.given
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
 import org.springframework.http.MediaType
-import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.post
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-internal class SubwayControllerTest(@Autowired val webTestClient: WebTestClient) {
+@ExtendWith(MockitoExtension::class)
+internal class SubwayControllerTest {
+
+    @Mock
+    lateinit var subwayService: SubwayService
+
+    @InjectMocks
+    lateinit var subwayController: SubwayController
+
+    lateinit var mockMvc: MockMvc
+
+    @BeforeEach
+    internal fun setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(subwayController)
+                .setCustomArgumentResolvers(SubwayInfoArgumentResolver())
+                .setControllerAdvice(SubwayControllerAdvice())
+                .build()
+    }
 
     @Test
     internal fun `정상적으로 지하철 도착정보를 가져온다`() {
-        val responseBody = webTestClient.post()
-                .uri("/api/subway")
-                .contentType(MediaType.APPLICATION_JSON)
-                .bodyValue(JsonFactory.createParams("station", "의정부"))
-                .exchange()
-                .expectStatus().isOk
-                .expectBody(String::class.java)
-                .returnResult().responseBody
+        given(subwayService.find(any()))
+                .willReturn(arrayListOf(BasicCard("testTitle", "content")))
+
+        mockMvc.post("/api/subway") {
+            accept = MediaType.APPLICATION_JSON
+            content = JsonFactory.createParams("station", "의정부")
+        }.andExpect {
+            status { isOk }
+            content { contentType(MediaType.APPLICATION_JSON) }
+            jsonPath("$.template.outputs[0].carousel.items[0].title") { value("testTitle") }
+        }.andDo {
+            print()
+        }
+    }
+
+    @Test
+    internal fun `올바르지 않은 지하철역이라면 예외가 발생한다`() {
+        given(subwayService.find(any())).willThrow(NotFoundSubwayException())
+
+        mockMvc.post("/api/subway") {
+            accept = MediaType.APPLICATION_JSON
+            content = JsonFactory.createParams("station", "의정부")
+        }.andExpect {
+            status { isOk }
+            content { contentType(MediaType.APPLICATION_JSON) }
+            jsonPath("$.template.outputs[0].simpleText.text") {
+                value("올바르지 않는 역이름 입니다.\nex) 잠실역(x) -> 잠실(o)")
+            }
+        }.andDo {
+            print()
+        }
     }
 }
