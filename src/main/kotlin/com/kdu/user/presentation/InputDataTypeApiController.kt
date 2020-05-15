6bfdc0f673d495cb71e0f85201a86897840591db
hyojaekim <hj00686@naver.com>
2020-05-15 22:17:19 +0900

    refactor: Apply requestMapping

--- src/main/kotlin/com/kdu/user/presentation/InputDataTypeApiController.kt
+++ src/main/kotlin/com/kdu/user/presentation/InputDataTypeApiController.kt
@@ -3,21 +3,19 @@ package com.kdu.user.presentation
 import com.kdu.user.application.InputDataTypeInternalService
 import com.kdu.user.presentation.dto.InputDataTypeRequestDto
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
+@RequestMapping("/api/user/data/type")
 class InputDataTypeApiController(private val inputDataTypeInternalService: InputDataTypeInternalService) {
 
-    @PostMapping("/api/user/data/type")
+    @PostMapping
     fun save(inputDataTypeRequestDto: InputDataTypeRequestDto): ResponseEntity<Void> {
         inputDataTypeInternalService.save(inputDataTypeRequestDto)
         return ResponseEntity.ok().build()
     }
 
-    @DeleteMapping("/api/user/data/type/{id}")
+    @DeleteMapping("/{id}")
     fun delete(@PathVariable id: Long): ResponseEntity<Void> {
         inputDataTypeInternalService.delete(id)
         return ResponseEntity.ok().build()
