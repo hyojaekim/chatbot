8433c10620ab7aa0f5d2477081f95ab2e777619b
hyojaekim <hj00686@naver.com>
2020-05-15 19:26:20 +0900

    feat: Vue.js init

---
+++ src/frontend/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import request from 'request'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  mounted() {
+    request("http://localhost:8080/api/hi", function (error, response, body) {
+      console.log('body : ', body);
+    })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
